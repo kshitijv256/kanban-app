999cb2f7c4ab120df56e65f84b17295be6a65948
Kshitij Verma <kshitijv250@gmail.com>
2023-06-01 20:29:07 +0530

    feat: Modal to edit board details

---
+++ src/components/board/EditBoard.tsx
@@ -0,0 +1,86 @@
+import { useState } from "react";
+import { Board, validateBoard } from "../../types/Board";
+import { Error } from "../../types/common";
+import { updateBoard } from "../../utils/apiUtils";
+
+export default function EditBoard(props: { board_id: number; board: Board }) {
+  const [form, setForm] = useState<Board>(props.board);
+  const [errors, setErrors] = useState<Error<Board>>({});
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setForm({ ...form, [name]: value });
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const errors = validateBoard(form);
+    if (Object.keys(errors).length > 0) {
+      setErrors(errors);
+    } else {
+      try {
+        const data = await updateBoard(props.board_id, form);
+        if (data) {
+          console.log("Board updated successfully");
+          window.location.reload();
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+  return (
+    <div className="bg-col4">
+      <h1 className="text-xl font-bold text-col1">Edit Board</h1>
+      <form className="py-4" onSubmit={handleSubmit}>
+        <div className="mb-4">
+          <label
+            htmlFor="title"
+            className="block text-col2 text-sm font-bold mb-2"
+          >
+            Title
+          </label>
+          <input
+            type="text"
+            name="title"
+            id="title"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            placeholder="Board Title"
+            value={form.title}
+            onChange={handleChange}
+          />
+          {errors.title && (
+            <p className="text-red-500 text-xs italic">{errors.title}</p>
+          )}
+        </div>
+        <div className="mb-4">
+          <label
+            htmlFor="description"
+            className="block text-col2 text-sm font-bold mb-2"
+          >
+            Description
+          </label>
+          <input
+            type="text"
+            name="description"
+            id="description"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            placeholder="Board Description"
+            value={form.description}
+            onChange={handleChange}
+          />
+          {errors.description && (
+            <p className="text-red-500 text-xs italic">{errors.description}</p>
+          )}
+        </div>
+        <div className="flex items-center justify-between">
+          <button
+            className="bg-col1/90 hover:bg-col1 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="submit"
+          >
+            Edit
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -20,7 +20,7 @@ export default function Modal(props: {
           leaveFrom="opacity-100"
           leaveTo="opacity-0"
         >
-          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+          <div className="fixed inset-0 bg-col5 bg-opacity-75 transition-opacity" />
         </Transition.Child>
 
         <div className="fixed inset-0 z-10 overflow-y-auto">
@@ -34,8 +34,8 @@ export default function Modal(props: {
               leaveFrom="opacity-100 translate-y-0 sm:scale-100"
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
-              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-                <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-col4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                <div className="bg-col4 px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
                   {children}
                 </div>
               </Dialog.Panel>
